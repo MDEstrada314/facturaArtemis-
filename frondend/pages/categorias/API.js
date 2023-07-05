e3d70c8329f3255299250da262b999d1941f572e
MDEstrada314 <mdelprograma@gmail.com>
2023-07-05 07:54:54 -0500

    feat: AHORA MAS

--- frondend/pages/categorias/API.js
+++ frondend/pages/categorias/API.js
@@ -1,11 +1,26 @@
 const urlCategorias = "http://localhost:4000/api/categorias"
 
+
 export const getCategorias= async ()=>{
     try {
         const categorias = await fetch (urlCategorias);
         const datoCategorias = await categorias.json();
         console.log(datoCategorias);
         return datoCategorias;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const nuevaCategorias = async(registroCategoria)=>{
+    try {
+        await fetch (urlCategorias,{
+            method:"POST",
+            body:JSON.stringify(registroCategoria),
+            headers:{"Content-Type": "application/json"}
+
+        })
     } catch (error) {
         console.log(error);
     }
